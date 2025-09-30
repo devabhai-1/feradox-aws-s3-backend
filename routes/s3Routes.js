1f0725a0d2f3eb7e66f97b223a2cc04860db15df
devabhai-1 <maxdevkishan@gmail.com>
2025-09-30 08:56:47 +0530

    Update s3Routes.js

--- routes/s3Routes.js
+++ routes/s3Routes.js
@@ -1,7 +1,12 @@
+// routes/s3Routes.js
 const express = require('express');
 const s3Controller = require('../controllers/s3Controller');
+const { verifyFirebaseToken } = require('../middleware/authMiddleware'); // प्रमाणीकरण मिडलवेयर इम्पोर्ट करें
 
 const router = express.Router();
-router.get('/get-url', s3Controller.getSignedUrl);
+
+// '/get-url' मार्ग के लिए, पहले verifyFirebaseToken मिडलवेयर चलाएं, फिर s3Controller.getSignedUrl
+// Android ऐप से 'fileName' और 'fileType' क्वेरी पैरामीटर के रूप में अपेक्षित हैं
+router.get('/get-url', verifyFirebaseToken, s3Controller.getSignedUrl);
 
 module.exports = router;

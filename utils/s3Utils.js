4d7e9dbeb9788c27bcf9d17f88205069769ccc77
devabhai-1 <maxdevkishan@gmail.com>
2025-10-07 11:38:09 +0530

    Update s3Utils.js

--- utils/s3Utils.js
+++ utils/s3Utils.js
@@ -1,5 +1,5 @@
 // utils/s3Utils.js
-const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl: awsGetSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
 const s3Client = new S3Client({
@@ -10,29 +10,13 @@ const s3Client = new S3Client({
   },
 });
 
-async function getSignedUrl(bucket, key, contentType, operation = 'getObject', expiresInSecond = 300) {
-  let command;
-  let params = { Bucket: bucket, Key: key };
-
+async function getSignedUrl(bucket, key, contentType, operation, expiresIn = 300) {
+  const params = { Bucket: bucket, Key: key };
   if (operation === 'putObject') {
-    if (!contentType) {
-      throw new Error("putObject ऑपरेशन के लिए ContentType आवश्यक है।");
-    }
     params.ContentType = contentType;
-    command = new PutObjectCommand(params);
-  } else if (operation === 'getObject') {
-    command = new GetObjectCommand(params);
-  } else {
-    throw new Error(`असमर्थित ऑपरेशन: ${operation}. 'getObject' या 'putObject' का उपयोग करें।`);
   }
-
-  try {
-    const signedUrl = await awsGetSignedUrl(s3Client, command, { expiresIn: expiresInSecond });
-    return signedUrl;
-  } catch (error) {
-    console.error(`S3 ${operation} के लिए हस्ताक्षरित URL उत्पन्न करने में त्रुटि (कुंजी: ${key}):`, error);
-    throw error;
-  }
+  const command = new PutObjectCommand(params);
+  return await awsGetSignedUrl(s3Client, command, { expiresIn });
 }
 
 module.exports = { getSignedUrl };

9c762ca945838ef129f40cba7fa97d1371bb4f63
devabhai-1 <maxdevkishan@gmail.com>
2025-10-07 11:43:01 +0530

    Update s3Routes.js

--- routes/s3Routes.js
+++ routes/s3Routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const s3Controller = require('../controllers/s3Controller');
 const { verifyFirebaseToken } = require('../middleware/authMiddleware');
 
+// GET /api/generate-s3-upload-url
 router.get('/generate-s3-upload-url', verifyFirebaseToken, s3Controller.generateUploadUrl);
+
+// POST /api/confirm-upload
 router.post('/confirm-upload', verifyFirebaseToken, s3Controller.confirmUpload);
 
 module.exports = router;
